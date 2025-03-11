6f75113186f5eadd530176b3665e4e8b61d67909
RadovanCipovic <radovan.cipovic@gmail.com>
2025-03-11 23:32:27 +0100

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function showsidebar() {
+let sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function hidesidebar() {
+    let sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
